0027cfc2c39abd965ef0eb7ad8e0b261c7061260
cat40 <johnthecat492@gmail.com>
2016-06-11 20:43:49 -0400

    Added root handling
    Can now handle root functions.
    Uses the inverse function principle to find the root function. If the
    function is simple enough, it can solve for y. If not, gives function as
    is.

--- mathsolver2.py
+++ mathsolver2.py
@@ -3,8 +3,9 @@ import math
 import sys
 import cat
 from itertools import product
+
 #takes a standard quadratic/cubic/quartic... regression
-def reg(xs, ys):
+def reg(xs, ys, retdeg=False):
     deg = len(xs)-1
     array = numpy.zeros((deg+1, deg+1))
     for i, x in enumerate(xs):
@@ -16,14 +17,47 @@ def reg(xs, ys):
         array[i][0] = y
     X = numpy.matrix(array)
     B = A.I*X
-    coeffs = [i[0] for i in B]
+    coeffs = [float(i[0]) for i in B]
     eq = ''
     for i, c in enumerate(coeffs):
+        #use for comparison; apparently floats are too imprecise
+        cr = round(c, 2)
         if c < 10**-10 and c > -(10**-10):
-            c = 0
-        if c != 0:
+            c = 0  
+        if cr == 1:
+            eq += 'x**%s+' % (deg-i)
+        elif cr:
             eq += '%s*x**%s+' % (float(c), deg-i)
-    return eq.rstrip('+')
+        #finds actual degree of equation
+        if c == deg:
+            deg -= 1
+    #formats equation to make more readable
+    eq = eq.rstrip('+')
+    
+    if retdeg:
+        return eq, deg, coeffs
+    else:
+        return eq
+
+#takes root regression
+#uses principal that root equations are the inverse of thier regular counterparts
+def root(xs, ys):
+    eq, deg, coeffs = reg(ys, xs, True)
+    eq = eq.replace('x', 'y')
+    #perform limited 'algebra'
+    if eq.count('y') == 1:
+        c = str(coeffs[len(coeffs)-1-deg])
+        c = '' if round(float(c), 2) == 1 else c
+        eqt = eq.split('+')
+        eql = eq.split('*')
+        r = eqt[len(eqt)-1] if eqt[len(eqt)-1] == eql[len(eql)-1] else 0
+        if r:
+            return str(c)+'x**(1/'+str(deg)+')+'+r
+        else:
+            return str(c)+'x**(1/'+str(deg)+')'
+    else:
+        return eq
+        
 
 def comb(r):
     if r == 0:
@@ -42,7 +76,7 @@ ys = raw_input("What is the output number set, separated with commaspaces?\n").s
 xs = [eval(x) for x in xs]
 ys = [eval(y) for y in ys]
 
-nums = ['x']+[a+b for a, b in product(list('0123456789'), repeat=2)]
+nums = ['x']+[str(float(a+b)) for a, b in product(list('0123456789'), repeat=2)]
 for i, n in enumerate(nums):
     nums[i] = n.lstrip('0')
 ops = ['+', '/', '-', '*', '**', '%', '+(', '-(', '*(', '/(', '%(' '**(', ')-', ')+', ')-', ')*', ')/', ')**', ')%']
@@ -50,30 +84,32 @@ ops = ['+', '/', '-', '*', '**', '%', '+(', '-(', '*(', '/(', '%(' '**(', ')-', 
 eq = reg(xs, ys)
 print eq
 if raw_input('does this match your equation? (y/n)').lower() != 'y':
-    limit = cat.input.getnum('input complexity limit')
-    print'recomputing. this may take a while.'
-    running = True
-    reps = 0
-    while running and reps <= limit:
-        print reps
-        for eq in comb(reps):
-            running = False
-            for x, y in zip(xs, ys):
-                try: z = eval(eq)
-                except:
-                    running = True
-                    pass
-                else:
-                    if z != y:
+    print root(xs, ys)
+    if raw_input('does this match your equlation (y/n)').lower() != 'y':
+        limit = cat.input.getnum('input complexity limit')
+        print'recomputing. this may take a while.'
+        running = True
+        reps = 0
+        while running and reps <= limit:
+            print reps
+            for eq in comb(reps):
+                running = False
+                for x, y in zip(xs, ys):
+                    try: z = eval(eq)
+                    except:
                         running = True
-                        break
+                        pass
+                    else:
+                        if z != y:
+                            running = True
+                            break
 
-            if not running:
-                print('your equation is\n %s' % eq)
-                if raw_input('Does this make sense? (y/n)\n').lower != 'y':
-                        running = True
-                else:
-                    running = False
-                    break
-        reps += 1
+                if not running:
+                    print('your equation is\n %s' % eq)
+                    if raw_input('Does this make sense? (y/n)\n').lower != 'y':
+                            running = True
+                    else:
+                        running = False
+                        break
+            reps += 1
 sys.exit()
